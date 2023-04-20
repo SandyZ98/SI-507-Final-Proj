832e14bcb4812d427a3878974466b79ffab8a1e5
Zhaoccdd_98 <70979610+SandyZ98@users.noreply.github.com>
2023-04-20 16:28:41 -0400

    Update topwords.py

--- topwords.py
+++ topwords.py
@@ -46,3 +46,34 @@ def topwords(posts):
     top_words = [word for word, freq in sorted_words[:5]]
     return top_words
 
+
+def top_comments(posts):
+    topcomment = max(posts.items(), key=lambda x: x[1]['Total Comments'])
+    post = view_posts(topcomment)
+    if post == None:
+        #return topcomment
+        return (f"The title of the post with the most comments is posted by user {topcomment[1]['ID']} with a number of {topcomment[1]['Total Comments']}")
+    else:
+        return (f"The title of the post with the most comments is posted by user {topcomment[1]['ID']} with a number of {topcomment[1]['Total Comments']}.The post is {post}")
+
+def view_posts(topcomment):
+    view = input("Please say Yes if you would like to view the post as well")
+    if view == "Yes":
+        if 'Post Text' != '':
+            return "But the post is empty..."
+        else:
+            return topcomment[1]['Post Text']
+    else:
+        return None
+
+
+def view_title(topcomment):
+    view = input("Please say Yes if you would like to view the title as well")
+    if view == "Yes":
+        if 'Title' != '':
+            return "But the title is empty..."
+        else:
+            return topcomment[1]['Title']
+    else:
+        return None
+
