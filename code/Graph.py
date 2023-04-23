253cdc35ce0778bdb522bdff9b86de4c392287d5
Zhaoccdd_98 <70979610+SandyZ98@users.noreply.github.com>
2023-04-23 11:48:27 -0400

    Update Graph.py

--- code/Graph.py
+++ code/Graph.py
@@ -1,7 +1,4 @@
 import matplotlib.pyplot as plt
-from collections import Counter
-from nltk.corpus import stopwords
-import string
 
 
 def Graphcomment(posts):
@@ -12,7 +9,7 @@ def Graphcomment(posts):
         posts (Any): The posts to be plotted.
     
     Returns:
-        Union[Callable, str, None]: Returns the plotted data if the user chooses '1', "Got it" if the user chooses '2', 
+        Plotted data if the user chooses '1', "Got it" if the user chooses '2', 
         and None if the user chooses '3'.
     """
     print("Would you also like to see the number of comments over time? '\n' 1 = Yes; '\n' 2 = No; '\n' 3 = Quit.")
@@ -34,7 +31,7 @@ def plot_comments_over_time(posts):
     Creates a plot of the number of comments over time for the given posts.
 
     Parameters:
-        posts (Dict[str, Any]): A dictionary of post objects, each containing a 'Created On' key with a date and a 
+        posts : A dictionary of post objects, each containing a 'Created On' key with a date and a 
         'Total Comments' key with the number of comments on the post.
 
     Returns:
@@ -67,38 +64,6 @@ def plot_comments_over_time(posts):
     plt.show()
 
 
-def top_words_frequency(posts, key_to_print):
-    """
-    Extracts the text data from the given posts and returns the top 7 most frequent unique words and their frequencies.
-
-    Parameters:
-        posts (dict): A dictionary containing posts as values.
-        key_to_print (str): The key in the post dictionary that contains the text data.
-
-    Returns:
-        list: A list of tuples containing the top 7 most frequent unique words and their frequencies.
-    """
-    # Extract the titles from the dictionary and flatten the list of lists
-    texts = [[word for word in post[key_to_print].lower().split()] for post_id, post in posts.items()]
-    tokens = [word for document in texts for word in document]
-
-    # Remove stop words and punctuations
-    stop_words = set(stopwords.words('english'))
-    tokens = [token for token in tokens if token not in stop_words and token not in string.punctuation]
-
-    # Count the frequency of each word
-    word_freq = Counter(tokens)
-
-    # Sort the words by frequency
-    sorted_words = sorted(word_freq.items(), key=lambda x: x[1], reverse=True)
-
-    # Return the top 5 unique words and their frequencies, without trailing punctuation
-    top_words_freq = [(word.rstrip(string.punctuation), freq) for word, freq in sorted_words[:7]]
-    top_words = list(set([word for word, freq in top_words_freq]))
-    top_words_freq = [(word, freq) for word, freq in top_words_freq if word in top_words]
-    top_words_freq = sorted(top_words_freq, key=lambda x: x[1], reverse=True)
-    return top_words_freq
-
 
 def plot_top_words_frequency(top_words_freq):
     """
@@ -118,9 +83,5 @@ def plot_top_words_frequency(top_words_freq):
     plt.xlabel("Words")
     plt.ylabel("Frequency")
     plt.show()
-
-
-
-
 
 
