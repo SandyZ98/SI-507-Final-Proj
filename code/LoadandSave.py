07e401b0e47ba1c3c0d983a93832d530c04c7f39
Zhaoccdd_98 <70979610+SandyZ98@users.noreply.github.com>
2023-04-23 11:49:41 -0400

    Update LoadandSave.py

--- code/LoadandSave.py
+++ code/LoadandSave.py
@@ -1,20 +1,28 @@
 import json
-import csv
+import os
 
-def read_json(filepath, encoding='utf-8'):
-    """Reads a JSON document, decodes the file content, and returns a list or dictionary if
-    provided with a valid filepath.
+def SaveInterface(post):
+    """
+    Provides a user interface for saving a given `post` object as a JSON file.
 
     Parameters:
-        filepath (str): path to file
-        encoding (str): name of encoding used to decode the file
+        post (dict/list): The `post` object to be saved as a JSON file.
 
     Returns:
-        dict/list: dict or list representations of the decoded JSON document
+        None
     """
-
-    with open(filepath, 'r', encoding=encoding) as file_obj:
-        return json.load(file_obj)
+    print("Would you like to save the data as json file?: '\n' 1 = Yes; '\n' 2 = No; '\n' 3 = Quit.")
+    userInput = input()
+    if userInput == '1':
+        filepath = input ("Please provide the file path: ")
+        write_json(filepath, post)
+    elif userInput == '2':
+        print("Okay!")
+    elif userInput == '3':
+        print('\nBye Bye!\n')
+        return
+    else:
+        print('Please input between 1, 2, 3')
 
 
 def write_json(filepath, data, encoding='utf-8', ensure_ascii=False, indent=2):
@@ -31,33 +39,8 @@ def write_json(filepath, data, encoding='utf-8', ensure_ascii=False, indent=2):
     Returns:
         None
     """
-
+    if not os.path.exists(os.path.dirname(filepath)):
+        print(f"Error: {os.path.dirname(filepath)} does not exist")
+        return
     with open(filepath, 'w', encoding=encoding) as file_obj:
         json.dump(data, file_obj, ensure_ascii=ensure_ascii, indent=indent)
-
-
-def read_csv_to_dicts(filepath, encoding='utf-8', newline='', delimiter=','):
-    """Accepts a file path, creates a file object, and returns a list of dictionaries that
-    represent the row values using the cvs.DictReader().
-
-    WARN: This function must be implemented using a list comprehension in order to earn points.
-
-    Parameters:
-        filepath (str): path to file
-        encoding (str): name of encoding used to decode the file
-        newline (str): specifies replacement value for newline '\n'
-                       or '\r\n' (Windows) character sequences
-        delimiter (str): delimiter that separates the row values
-
-    Returns:
-        list: nested dictionaries representing the file contents
-     """
-
-    with open(filepath, 'r', newline=newline, encoding=encoding) as file_obj:
-        # data = []
-        # reader = csv.DictReader(file_obj, delimiter=delimiter)
-        # for line in reader:
-        #     data.append(line) # OrderedDict() | alternative: data.append(dict(line))
-
-        reader = csv.DictReader(file_obj, delimiter=delimiter)
-        return [line for line in reader]
